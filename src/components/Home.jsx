d48ddbacbfc5677bcf845bfb48035f3ce7c76603
praveen.km@celestialsys.com <praveen@CELSYSBLR000483.local>
2026-05-23 21:19:05 +0530

    home page shirts deleted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,29 +33,7 @@ function Home() {
         <div className="flex">
             <h1 className="font-bold mt-6 mb-6 text-2xl ">Most Selled Jersies</h1>
         </div>
-        <Link to={'/shirts'}>
         
-            <div className="grid grid-rows-auto grid-cols-2  sm:grid-cols-6 mt-3  h-70 w-full">
-                <div className=" w-45 h-45 items-center-safe">
-                    <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/6/n/m/s-mi045-lini-original-imahayebdhbyvgfy.jpeg?q=70" alt="" />
-                </div>
-                <div className=" w-45 h-45">
-                    <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/h/v/2/xxl-fk-rcb-26-polo-m2-2xs-royal-hitters-original-imahm2yrsbhhz5vm.jpeg?q=70" alt="" />
-                </div>
-                <div className="w-45 h-45">
-                        <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/x/g/9/s-cskk-2026-msd-yellow-cutis-original-imahmmvtwj2ctsvt.jpeg?q=70" alt="" />
-                </div>
-                <div className=" w-45 h-45">
-                        <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/n/5/c/xxl-team-india-t20-cricket-jersey-technolink-original-imahjrt7e2gpy42w.jpeg?q=70" alt="" />
-                </div>
-                <div className=" w-45 h-45">
-                    <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/f/v/r/s-fk-ronaldo7-0-dostitch-original-imah7jvbkfzsgahf.jpeg?q=70" alt="" />
-                </div>
-                <div className=" w-45 h-45">
-                    <img src="https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/b/k/i/l-080-homearg2022-m10-be-smv-original-imagt6emgvhjvszp.jpeg?q=70" alt="" />
-                </div>
-            </div>
-        </Link>
     </div>
   );
 }
